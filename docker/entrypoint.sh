8dba8f630d28069400c6208599717d56ef48c829
Anton Melnikov <thenotnami@gmail.com>
2019-05-09 09:38:08 -0500

    Download the model inside the container when starting

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip -O bert_model.zip && \
+mkdir /model && \
+unzip -j bert_model.zip -d /model && \
 bert-serving-start -num_worker=$1 -model_dir /model -http_port 8125
