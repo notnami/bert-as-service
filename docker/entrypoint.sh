62b3fac357551951b266646514e68b45b3889443
Anton Melnikov <thenotnami@gmail.com>
2019-06-05 09:49:44 -0500

    Add argument for maximum sequence length

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,4 @@
 wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip -O bert_model.zip && \
 mkdir /model && \
 unzip -j bert_model.zip -d /model && \
-bert-serving-start -num_worker=$1 -model_dir /model -http_port 8125
+bert-serving-start -num_worker=$1 -model_dir /model -http_port 8125 -max_seq_len $2 
